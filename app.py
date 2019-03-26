1228c3648c2374126a2fd90f6bc723bd71e2eaac
Michael <michael@Michaels-MacBook-Pro.local>
2019-03-26 10:50:47 -0700

    Manual start/stop

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, request
+import sys
+sys.path.insert(0, "..")
+import time
+
+from opcua import Client
+from opcua import ua
 
 app = Flask(__name__)
 
@@ -16,9 +22,27 @@ def manMode():
 @app.route('/manualSubmit', methods=['POST'])
 def mresult():
     if request.method == 'POST':
+        client = Client("opc.tcp://192.168.0.211:4870")
         result = request.form
         data = request.form.to_dict()
         print(data)
+        print(data['runmode'])
+        if data['runmode'] == "False":
+            print("STOP")
+            client.connect()
+            estop = client.get_node("ns=3;s=M_E_Stop")
+            estop.set_value(True)
+            sendorder= client.get_node("ns=3;s=M_Send_Order")
+            sendorder.set_value(False)
+            client.disconnect()
+        else:
+            print("START")
+            client.connect()
+            estop = client.get_node("ns=3;s=M_E_Stop")
+            estop.set_value(False)
+            sendorder =client.get_node("ns=3;s=M_Send_Order")
+            sendorder.set_value(True)
+            client.disconnect()
         return render_template("result.html", result=result)
 
 
